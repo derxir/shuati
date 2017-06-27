140944a51f9ebcd363ce532ac6416e8f00a46bfd
Justin <jiawei.yue1991@me.com>
2017-06-27 23:23:44 +0800

    add more.

---
+++ src/main/java/string/MaxSubArraySum.java
@@ -0,0 +1,29 @@
+package string;
+
+/**
+ * Created by justin on 6/27/17.
+ * <p>
+ * DP. Denote S[i] sum of max sub array in A[i]
+ * then, S[i+1] = max(S[i] + A[i+1], A[i+1]).
+ */
+public class MaxSubArraySum {
+
+    static int findMaxSub(int[] array) {
+        int result = array[0];
+        int sum = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if (sum > 0) {
+                sum += array[i];
+            } else {
+                sum = array[i];
+            }
+
+            if (sum > result) {
+                result = sum;
+            }
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/string/MaxSubArraySumTest.java
@@ -0,0 +1,29 @@
+package string;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static string.MaxSubArraySum.findMaxSub;
+
+/**
+ * Created by justin on 6/27/17.
+ */
+public class MaxSubArraySumTest {
+
+    @Test
+    public void main() {
+
+        int[] a1 = new int[]{1, 2, 3, 4, -4, -2, 1};
+        assertEquals(10, findMaxSub(a1));
+
+        int[] a2 = new int[]{1, 2, 3, -4, 4, 2, -1};
+        assertEquals(8, findMaxSub(a2));
+
+        int[] a3 = new int[]{-1, -2, -3, 4, 5, 6};
+        assertEquals(15, findMaxSub(a3));
+
+
+    }
+
+
+}
