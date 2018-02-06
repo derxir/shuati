f4ee7350e9b985944a05c2bb50cc8a5bf276b047
Justin <jiawei.yue1991@me.com>
2018-02-06 18:44:52 +0000

    add two stack queue

---
+++ src/main/java/linear/list/TwoStackQueue.java
@@ -0,0 +1,45 @@
+package linear.list;
+
+import java.util.Stack;
+
+/**
+ * Created by justin on 2/6/18.
+ * <p>
+ * implement a queue using two stacks
+ */
+public class TwoStackQueue {
+
+    private Stack<Integer> stackPush;
+    private Stack<Integer> stackPop;
+
+    public TwoStackQueue() {
+        stackPush = new Stack<>();
+        stackPop = new Stack<>();
+    }
+
+    public void add(int i) {
+        stackPush.push(i);
+    }
+
+    public int poll() {
+        if (stackPush.empty() && stackPop.empty()) {
+            throw new RuntimeException("Queue is empty");
+        } else if (stackPop.empty()) {
+            while(!stackPush.empty()){
+                stackPop.push(stackPush.pop());
+            }
+        }
+        return stackPop.pop();
+    }
+
+    public int peek() {
+        if (stackPush.empty() && stackPop.empty()) {
+            throw new RuntimeException("Queue is empty");
+        } else if (stackPop.empty()) {
+            while(!stackPush.empty()){
+                stackPop.push(stackPush.pop());
+            }
+        }
+        return stackPop.peek();
+    }
+}
---
+++ src/test/java/linear/list/TwoStackQueueTest.java
@@ -0,0 +1,26 @@
+package linear.list;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by justin on 2/6/18.
+ */
+public class TwoStackQueueTest {
+    @Test
+    public void testQueue(){
+        TwoStackQueue queue = new TwoStackQueue();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        assertEquals(queue.poll(), 1);
+        assertEquals(queue.poll(), 2);
+        queue.add(4);
+        queue.add(5);
+        assertEquals(queue.poll(), 3);
+        queue.add(6);
+        assertEquals(queue.peek(), 4);
+    }
+
+}
