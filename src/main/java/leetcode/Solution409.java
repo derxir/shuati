f306f0677c86d808de58905d3fdda9bec172b7b7
Justin <jiawei.yue1991@me.com>
2018-06-16 00:34:50 +0100

    409: using char array as to hashset. use char as index.

--- src/main/java/leetcode/Solution409.java
+++ src/main/java/leetcode/Solution409.java
@@ -25,6 +25,19 @@ class Solution409 {
         return set.isEmpty() ? 2 * res : 2 * res + 1;
     }
 
+    static int longestPalindrome2(String s){
+        int[] bit = new int[128];
+        for(char c: s.toCharArray()){
+            bit[c]++;
+        }
+        int res = 0;
+        for ( int count : bit){
+            res += count / 2 * 2;
+            if( res % 2 == 0 && count % 2 == 1) res++;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         String s = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
         System.out.println(longestPalindrome(s));
