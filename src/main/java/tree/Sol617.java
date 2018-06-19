5ae37d889269257cee9b081b2b76de3cf029a334
Justin <jiawei.yue1991@me.com>
2018-06-19 23:41:42 +0100

    617 add stack based sol

--- src/main/java/tree/Sol617.java
+++ src/main/java/tree/Sol617.java
@@ -1,5 +1,7 @@
 package tree;
 
+import java.util.Stack;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -55,6 +57,31 @@ class Sol617 {
         return t1;
     }
 
+    public static TreeNode mergeTrees3(TreeNode t1, TreeNode t2) {
+        if (t1 == null)
+            return t2;
+        Stack< TreeNode[] > stack = new Stack < > ();
+        stack.push(new TreeNode[] {t1, t2});
+        while (!stack.isEmpty()) {
+            TreeNode[] t = stack.pop();
+            if (t[0] == null || t[1] == null) {
+                continue;
+            }
+            t[0].val += t[1].val;
+            if (t[0].left == null) {
+                t[0].left = t[1].left;
+            } else {
+                stack.push(new TreeNode[] {t[0].left, t[1].left});
+            }
+            if (t[0].right == null) {
+                t[0].right = t[1].right;
+            } else {
+                stack.push(new TreeNode[] {t[0].right, t[1].right});
+            }
+        }
+        return t1;
+    }
+
     public static void main(String[] args) {
 //        [1,2,null,3]
 //        [1,null,2,null,3]
@@ -66,5 +93,7 @@ class Sol617 {
         t2.right.right = new TreeNode(3);
 
         TreeNode t = mergeTrees(t1, t2);
+        TreeNode tt = mergeTrees2(t1, t2);
+        TreeNode ttt = mergeTrees3(t1, t2);
     }
 }
