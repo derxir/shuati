3667923fe6ae56e6f53080b126c5b5cfb44fc652
Justin <jiawei.yue1991@me.com>
2018-06-19 23:31:42 +0100

    leetcode 617 better solution

--- src/main/java/tree/Sol617.java
+++ src/main/java/tree/Sol617.java
@@ -44,6 +44,17 @@ class Sol617 {
         return null;
     }
 
+    public static TreeNode mergeTrees2(TreeNode t1, TreeNode t2) {
+        if (t1 == null)
+            return t2;
+        if (t2 == null)
+            return t1;
+        t1.val += t2.val;
+        t1.left = mergeTrees2(t1.left, t2.left);
+        t1.right = mergeTrees2(t1.right, t2.right);
+        return t1;
+    }
+
     public static void main(String[] args) {
 //        [1,2,null,3]
 //        [1,null,2,null,3]
