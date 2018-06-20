cc96f8154d058512ca8e6b64949beabb0fb726b6
Justin <jiawei.yue1991@me.com>
2018-06-20 23:43:05 +0100

    simple dfs implementation

--- src/main/java/graph/Graph.java
+++ src/main/java/graph/Graph.java
@@ -1,5 +1,6 @@
 package graph;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 /**
@@ -17,7 +18,38 @@ public class Graph {
         }
     }
 
-    public void addEdge(int u, int v){
+    public void addEdge(int u, int v) {
         adj[u].add(v);
+    }
+
+    private void DFSUtil(int v, boolean visited[]) {
+        visited[v] = true;
+        System.out.print(v + " ");
+
+        Iterator<Integer> i = adj[v].listIterator();
+        while (i.hasNext()) {
+            int n = i.next();
+            if (!visited[n])
+                DFSUtil(n, visited);
+        }
+    }
+
+    public void DFS(int v) {
+        boolean visited[] = new boolean[V];
+
+        DFSUtil(v, visited);
+    }
+
+    public static void main(String args[]) {
+        Graph g = new Graph(4);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+
+        g.DFS(0);
     }
 }
